3882d13d9e7ac4b9617d49614e1bd44d398acabc
Danny-PC-i7\Danny <keataren>
2018-04-20 23:22:15 -0400

    - Added card snapback to hand when put onto board. - Added internal card states to be used later.

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -15,8 +15,14 @@ public class Card : MonoBehaviour {
 	public float defence_time;
 	public float casting_time;
 
+	public enum card_states {Hand,Waiting,Attacking,Cooldown};
+	public card_states current_state;
+
+	public Player_Hand held_in;
+
 	//Making card draggable//
 	private Vector3 offset;
+
 
 	// Use this for initialization
 	void Start () 
@@ -30,7 +36,8 @@ public class Card : MonoBehaviour {
 		
 	}
 
-	void OnMouseDown() {
+	void OnMouseDown() 
+	{
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
 	}
 
@@ -39,5 +46,17 @@ public class Card : MonoBehaviour {
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 		transform.position = curPosition;
+	}
+
+	void OnMouseUp()
+	{
+		if (current_state == card_states.Hand){
+			held_in.arrange_cards();
+			//TODO: Check to see if we're in the play area of the map. In that case play the card, remove it from your hand and and assign it to a different holder to arrange.
+		}
+
+		else if (current_state == card_states.Waiting){
+			//TODO: Check to see if we're being assigned to another creature or face. If so, start attacking, defending, ect.
+		}
 	}
 }
--- Assets/Player_Hand.cs
+++ Assets/Player_Hand.cs
@@ -15,17 +15,33 @@ public class Player_Hand : MonoBehaviour {
 	void Start () 
 	{
 		for (int counter = 0; counter < starting_hand_size; counter++){
-			Vector3 card_position = this.transform.position;
-			card_position.x += -((starting_hand_size/2f) * xoffset_per_card) + xoffset_per_card*counter;
-			GameObject new_card = Instantiate(basic_card_prefab,card_position,this.transform.rotation);
+			GameObject new_card = Instantiate(basic_card_prefab);
+			new_card.GetComponent<Card>().current_state = Card.card_states.Hand;
+			new_card.GetComponent<Card>().held_in = this;
 			current_hand.Add(new_card);
 		}
+		arrange_cards();
 		//TODO: instantiate a bunch of cards at the start of the game
 		//TODO: add a "Draw" method that draws cards from the side of the board instead of just instantiating them in your hand.
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
+
+	public void arrange_cards() //Take all of the cards currently in our hand and arrange them.
+	{
+		int counter = 0;
+		foreach (GameObject next_card in current_hand)
+		{
+			Vector3 card_position = this.transform.position;
+			card_position.x += -((current_hand.Count/2f) * xoffset_per_card) + xoffset_per_card*counter;
+			next_card.transform.position = card_position;
+			counter++;
+		}
+	}
+
+
 }
