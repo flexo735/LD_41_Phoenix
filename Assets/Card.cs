9c2b16f41965bef565481c86e8b12d392627716f
Danny-PC-i7\Danny <keataren>
2018-04-20 22:17:34 -0400

    - Added player empty object, to be filled with logic for holding a hand of cards. - Added basic card prefab - Added ability to drag cards aroudn the game board.

---
+++ Assets/Card.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Card : MonoBehaviour {
+
+
+	//Key card stats//
+	public int attack_power;
+	public int defence_power;
+	public int health;
+	public float attack_time;
+	public float defence_time;
+	public float casting_time;
+
+	//Making card draggable//
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnMouseDown() {
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+}
