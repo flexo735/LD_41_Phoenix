e9d01d9925cf2985037ead35f31c245539c1bf3a
Danny-PC-i7\Danny <keataren>
2018-04-20 22:37:55 -0400

    Removed artifacted card back, replaced with high-res card back and front. Added hand generation at start of round.

--- Assets/Player_Hand.cs
+++ Assets/Player_Hand.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Player_Hand : MonoBehaviour {
 
+	public GameObject basic_card_prefab;
 
+	public List <GameObject> current_hand = new List<GameObject>();
+
+	public float xoffset_per_card = 0.5f;
+	public int starting_hand_size = 6;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		for (int counter = 0; counter < starting_hand_size; counter++){
+			Vector3 card_position = this.transform.position;
+			card_position.x += -((starting_hand_size/2f) * xoffset_per_card) + xoffset_per_card*counter;
+			GameObject new_card = Instantiate(basic_card_prefab,card_position,this.transform.rotation);
+			current_hand.Add(new_card);
+		}
+		//TODO: instantiate a bunch of cards at the start of the game
+		//TODO: add a "Draw" method that draws cards from the side of the board instead of just instantiating them in your hand.
 	}
 	
 	// Update is called once per frame
