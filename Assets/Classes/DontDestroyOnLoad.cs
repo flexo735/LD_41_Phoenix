c1d625e11cfa8f125851f4681883decfbbd8744d
Timothy Beck <timothy.beck@uoit.net>
2018-04-22 17:10:51 -0400

    The game now has music, so your ears won't be bored. They might bleed, but they won't be bored.

---
+++ Assets/Classes/DontDestroyOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+	public int id;
+	public static Hashtable list;
+	void Awake(){
+		if(list == null)
+			list = new Hashtable();
+		if(list.Contains(id))
+			{GameObject.Destroy(gameObject); return;}
+
+		list.Add(id, gameObject);
+		DontDestroyOnLoad(gameObject);
+	}
+}
